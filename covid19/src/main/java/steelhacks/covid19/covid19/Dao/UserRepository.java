136a0168ce021147778fd8220ed864a9bacd0877
yiz132 <yiz132@pitt.edu>
2020-02-15 01:26:50 -0500

    connect mongoDB

--- covid19/src/main/java/steelhacks/covid19/covid19/Dao/UserRepository.java
+++ covid19/src/main/java/steelhacks/covid19/covid19/Dao/UserRepository.java
@@ -1,13 +1,14 @@
 package steelhacks.covid19.covid19.Dao;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import steelhacks.covid19.covid19.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends MongoRepository<User, Integer> {
     public User findByUsernameAndPassword(String username, String password);
 
     public User  findByUsername(String username);
+    public User getOne(Integer id);
 }
